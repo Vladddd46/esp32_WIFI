86df1e620695ec7d6d644354f9abf3ff6fbb512f
Vladyslav <Vladddd46>
2020-11-04 21:06:30 +0200

    t02 improved

--- t02/main/ghttp_command.c
+++ t02/main/ghttp_command.c
@@ -35,51 +35,109 @@ static void inline uart_print(char *msg, bool newline, char *color) {
 
 
 
-static void dns_found(const char *name, const ip_addr_t *ipaddr, void *callback_arg) {
+static void dns_found(char *name, ip_addr_t *ipaddr, void *callback_arg) {
+	printf(">>%s\n", name);
     if (ipaddr == NULL) {
+    	printf("!%s\n", name);
         DNSFound = false;
     }
     else {
+    	printf("2.%s\n", name);
 	    ip_Addr = *ipaddr;
 	    DNSFound = true;
     }
 }
 
 
-
-
-
-void ghttp_command(char **cmd, int len) {
-	if (len != 2) {
-		uart_print(GHTTP_WRONG_SYNTAX, 1, RED_TEXT);
-		return;
-	}
-	dns_gethostbyname(cmd[1], &ip_Addr, dns_found, NULL);
-
+/*
+ * Takes host_name as argument and returns
+ * ip address of this host.
+ * In case of no ip associated with such host, return NULL.
+ */
+static char *resolve_ip_by_host_name(char *host_name) {
+	printf("%s\n", host_name);
+	dns_gethostbyname(host_name, &ip_Addr, dns_found, NULL);
 	int num_of_ms = 0;
     while(!DNSFound) {
-        if (num_of_ms == 50) {
+        if (num_of_ms == 100) {
             break;
         }
         num_of_ms +=1;
         vTaskDelay(1);
     }
 
+    printf("%s\n", host_name);
     char ip_adress[32];
     bzero(ip_adress, 32);
-
-	if (DNSFound) {
+    char *res;
+    if (DNSFound) {
 		sprintf(ip_adress, "%i.%i.%i.%i", 
-            ip4_addr1(&ip_Addr.u_addr.ip4), 
-            ip4_addr2(&ip_Addr.u_addr.ip4), 
-            ip4_addr3(&ip_Addr.u_addr.ip4), 
-            ip4_addr4(&ip_Addr.u_addr.ip4));
-
-		uart_print(ip_adress, 1, GREEN_TEXT);
+                ip4_addr1(&ip_Addr.u_addr.ip4), 
+                ip4_addr2(&ip_Addr.u_addr.ip4), 
+                ip4_addr3(&ip_Addr.u_addr.ip4), 
+                ip4_addr4(&ip_Addr.u_addr.ip4));
+		res = mx_string_copy(ip_adress);
+		DNSFound = false;
+		return res;
 	}
-	else {
+	DNSFound = false;
+	return NULL;
+}
+
+/* @ Splits url into {host_name, path/to/document}
+ * Takes url as argument: host_name/path/to/document.
+ * Returns array of 2 elements {host_name, path/to/document}
+ */
+static char **split_url(char *url) {
+	char **arr = mx_strarr_new(2);
+	char host_name[500];
+	bzero(host_name, 500);
+	int i = 0;
+	int index = 0;
+	for (; url[i]; ++i) {
+		if (url[i] == '/') {
+			i += 1;
+			break;
+		}
+		host_name[i] = url[i];
+	}
+	char path_to_doc[500];
+	bzero(path_to_doc, 500);
+	index = 0;
+	for (; url[i]; ++i) {
+		path_to_doc[index] = url[i];
+		index++;
+	}
+	arr[0] = mx_string_copy(host_name);
+	arr[1] = mx_string_copy(path_to_doc);
+	return arr;
+}
+
+void ghttp_command(char **cmd, int len) {
+	if (len != 2) {
+		uart_print(GHTTP_WRONG_SYNTAX, 1, RED_TEXT);
+		return;
+	}
+	char **splited_url = split_url(cmd[1]);
+    char *ip_adress = resolve_ip_by_host_name(splited_url[0]);
+
+    if (ip_adress != NULL) {
+    	uart_print(ip_adress,    1, GREEN_TEXT);
+    }
+    else {
 		uart_print(NO_SUCH_HOST, 1, RED_TEXT);
 	}
 
 
+
+	// freeing memory.
+	for (int i = 0; splited_url[i] ; ++i) {
+		free(splited_url[i]);
+	}
+	free(splited_url);
+	if (ip_adress != NULL) {
+		free(ip_adress);
+	}
 }
+
+
