ddcb0f0d07cf8e54e755879908b45f863b5a629d
Vladyslav <Vladddd46>
2020-11-24 17:53:57 +0200

    post request data parse made

--- creative/components/my_http_server/my_http_server.c
+++ creative/components/my_http_server/my_http_server.c
@@ -2,79 +2,88 @@
 
 
 
+// @ Taskes SSID and wrapps it in html from.
+static char *form_wrapper(char *ssid_name) {
+    char res[400];
+    bzero(res, 400);
 
-// static char *tag_wrapper(char *str, char *tag) {
-//     char res[100];
-//     bzero(res, 100);
-
-//     sprintf(res, "<%s>%s</%s>", tag, str, tag);
-//     char *result = mx_string_copy(res);
-//     return result;
-// }
-
-static char *form_wrapper(char *ssid_name, int id) {
-    char res[700];
-    bzero(res, 700);
-
-    sprintf(res, "<form enctype=\"text/plain\" action=\"/\" method=\"post\"><p>%s<input name=\"%s\" value=\"\" type=\"text\" size=\"40\" placeholder=\"password\">\
-                        <input type=\"submit\"></p></form>", ssid_name, ssid_name);
+    sprintf(res, "<form enctype=\"text/plain\" action=\"/\" method=\"post\">\
+                    <p>%s <input name=\"%s\" value=\"\" type=\"text\" size=\"40\" placeholder=\"password\">\
+                        <input type=\"submit\">\
+                    </p>\
+                  </form>", ssid_name, ssid_name);
     char *result = mx_string_copy(res);
     return result;
 }
 
 
 
+/* @Handles http get request URL: /
+ *  1. Scans all available wifi networks. 
+ *  2. Froms html response, where there are forms 
+ *     for selecting wifi ssid and entering password.
+ *  3. Sends response.
+ */
 esp_err_t get_handler(httpd_req_t *req) {
     char response[10000];
     bzero(response, 10000);
     
-    printf("scanning\n");
+    printf("scanning wifi_networks...\n");
     char **avaliable_networks = scan_wifi_networks();
-    printf("end scann\n");
+    printf("scanning end\n");
     
-    int id = 0;
+    // response formation.
+    char *wrapped_str;
     int index = 0;
     for (int i = 0; avaliable_networks[i]; ++i) {
-        char *wrapped_str = form_wrapper(avaliable_networks[i], id);
+        wrapped_str = form_wrapper(avaliable_networks[i]);
         for (int j = 0; wrapped_str[j]; ++j) {
            response[index] = wrapped_str[j];
            index++;
         }
         free(wrapped_str);
-        id++;
     }
+
+    // sending response.
     httpd_resp_send(req, response, HTTPD_RESP_USE_STRLEN);
+
     // freeing memmory.
-    for (int i = 0; avaliable_networks[i] ; ++i) {
-        free(avaliable_networks[i]);
-    }
+    for (int i = 0; avaliable_networks[i]; ++i) {free(avaliable_networks[i]);}
     free(avaliable_networks);
     return ESP_OK;
 }
 
 
 
+/* @ Received data, user entered to connect to wifi.
+ * Received data structure:
+ *  wifi_ssid=wifi_password
+ *
+ */
 esp_err_t post_handler(httpd_req_t *req) {
-    printf("123\n");
-    char content[10000];
-    bzero(content, 10000);
+    char content[100];
+    bzero(content, 100);
     size_t recv_size = sizeof(content);
-
-    printf("2\n");
     int ret = httpd_req_recv(req, content, recv_size);
-    printf("3\n");
     if (ret <= 0) {
-        printf("%d\n", ret);
         if (ret == HTTPD_SOCK_ERR_TIMEOUT) {
-            printf("ашибка\n");
             httpd_resp_send_408(req);
         }
         return ESP_FAIL;
     }
-    printf(">>>>>>>%s\n", content);
-    const char resp[] = "URI POST Response";
-    printf("6\n");
-    httpd_resp_send(req, "OK", HTTPD_RESP_USE_STRLEN);
+    char *msg = "OK";
+    httpd_resp_send(req, msg, strlen(msg));
+    
+    char **splt_data = mx_strsplit(content, '=');
+    char *wifi_ssid  = mx_string_copy(splt_data[0]);
+    char *wifi_pass  = mx_string_copy(splt_data[1]);
+
+    for (int i = 0; splt_data[i]; ++i) {
+        free(splt_data[i]);
+    }
+    free(splt_data);
+
+    printf("%s with pass %s\n", wifi_ssid, wifi_pass);
     return ESP_OK;
 }
 
@@ -97,7 +106,7 @@ httpd_uri_t uri_post = {
 };
 
 
-
+// Initialize simple http server.
 httpd_handle_t http_server_init() {
     httpd_config_t config = HTTPD_DEFAULT_CONFIG();
     config.stack_size = 80000;
@@ -107,10 +116,5 @@ httpd_handle_t http_server_init() {
         httpd_register_uri_handler(server, &uri_get);
         httpd_register_uri_handler(server, &uri_post);
     }
-
-    while(1) {
-        vTaskDelay(1);
-    }
-
     return server;
 }
