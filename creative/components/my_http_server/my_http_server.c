d632b3232d30cdc6ddd66cdc303a6cde82c7c533
Vladyslav <Vladddd46>
2020-11-24 17:25:02 +0200

    httpserver improved

--- creative/components/my_http_server/my_http_server.c
+++ creative/components/my_http_server/my_http_server.c
@@ -3,21 +3,21 @@
 
 
 
-static char *tag_wrapper(char *str, char *tag) {
-    char res[100];
-    bzero(res, 100);
+// static char *tag_wrapper(char *str, char *tag) {
+//     char res[100];
+//     bzero(res, 100);
 
-    sprintf(res, "<%s>%s</%s>", tag, str, tag);
-    char *result = mx_string_copy(res);
-    return result;
-}
+//     sprintf(res, "<%s>%s</%s>", tag, str, tag);
+//     char *result = mx_string_copy(res);
+//     return result;
+// }
 
-static char *form_wrapper(char *ssid_name) {
-    char res[300];
-    bzero(res, 300);
+static char *form_wrapper(char *ssid_name, int id) {
+    char res[700];
+    bzero(res, 700);
 
-    sprintf(res, "<form name=\"test\" method=\"post\"><p>%s<input type=\"text\" size=\"40\" placeholder=\"password\">\
-            <input type=\"submit\" value=\"Connect\"></p></form>", ssid_name);
+    sprintf(res, "<form enctype=\"text/plain\" action=\"/\" method=\"post\"><p>%s<input name=\"%s\" value=\"\" type=\"text\" size=\"40\" placeholder=\"password\">\
+                        <input type=\"submit\"></p></form>", ssid_name, ssid_name);
     char *result = mx_string_copy(res);
     return result;
 }
@@ -25,32 +25,29 @@ static char *form_wrapper(char *ssid_name) {
 
 
 esp_err_t get_handler(httpd_req_t *req) {
-    char response[800];
-    bzero(response, 800);
+    char response[10000];
+    bzero(response, 10000);
     
     printf("scanning\n");
     char **avaliable_networks = scan_wifi_networks();
     printf("end scann\n");
     
+    int id = 0;
     int index = 0;
     for (int i = 0; avaliable_networks[i]; ++i) {
-        printf("1\n");
-        char *wrapped_str = form_wrapper(avaliable_networks[i]);
-        printf("2\n");
+        char *wrapped_str = form_wrapper(avaliable_networks[i], id);
         for (int j = 0; wrapped_str[j]; ++j) {
            response[index] = wrapped_str[j];
            index++;
         }
         free(wrapped_str);
+        id++;
     }
-    printf("123456789\n");
     httpd_resp_send(req, response, HTTPD_RESP_USE_STRLEN);
-    printf("hello world\n");
     // freeing memmory.
     for (int i = 0; avaliable_networks[i] ; ++i) {
         free(avaliable_networks[i]);
     }
-    printf("yes\n");
     free(avaliable_networks);
     return ESP_OK;
 }
@@ -58,18 +55,26 @@ esp_err_t get_handler(httpd_req_t *req) {
 
 
 esp_err_t post_handler(httpd_req_t *req) {
-    char content[100];
+    printf("123\n");
+    char content[10000];
+    bzero(content, 10000);
     size_t recv_size = sizeof(content);
 
+    printf("2\n");
     int ret = httpd_req_recv(req, content, recv_size);
+    printf("3\n");
     if (ret <= 0) {
+        printf("%d\n", ret);
         if (ret == HTTPD_SOCK_ERR_TIMEOUT) {
+            printf("ашибка\n");
             httpd_resp_send_408(req);
         }
         return ESP_FAIL;
     }
+    printf(">>>>>>>%s\n", content);
     const char resp[] = "URI POST Response";
-    httpd_resp_send(req, resp, HTTPD_RESP_USE_STRLEN);
+    printf("6\n");
+    httpd_resp_send(req, "OK", HTTPD_RESP_USE_STRLEN);
     return ESP_OK;
 }
 
@@ -95,11 +100,16 @@ httpd_uri_t uri_post = {
 
 httpd_handle_t http_server_init() {
     httpd_config_t config = HTTPD_DEFAULT_CONFIG();
+    config.stack_size = 80000;
     httpd_handle_t server = NULL;
 
     if (httpd_start(&server, &config) == ESP_OK) {
         httpd_register_uri_handler(server, &uri_get);
         httpd_register_uri_handler(server, &uri_post);
+    }
+
+    while(1) {
+        vTaskDelay(1);
     }
 
     return server;
